654baac0d4641fc3355ab9b019072d7a5261689c
DisTv1 <154090544+DisTv1@users.noreply.github.com>
2024-03-12 19:28:46 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,336 @@
+
+
+<head>
+<link rel="icon" type="image/svg+xml" href="http://distv.hopto.org:8000/up/upload/DD.png">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@500&family=Roboto:wght@300&display=swap" rel="stylesheet" />
+<title>DisTv</title>
+<meta content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" name="viewport" />
+ <meta charset="UTF-8">
+  
+  
+  
+ <!-- إعلانات -->
+<table width="100%" cellpadding="2" cellspacing="2" border="2" align="center">
+</td><td>
+ <table class="tborder" cellpadding="0" cellspacing="" border="2" width="100%" align="right">
+<tr><td class="thead" align="center">
+
+</a>
+</font>
+</td></tr>
+<tr><td class="alt1" align="right">
+<MARQUEE onmouseover=this.stop() onmouseout=this.start() scrollAmount=5 scrollDelay=50 direction=right><table width="100%" cellpadding="20" cellspacing="10" border="0" align="right">
+<a href="http://distv.hopto.org:8000/disbook/"><img src ="http://distv.hopto.org:8000/up/upload/book.jpg" height="60px" width="150px" vspace="5px"></a>
+<a href="http://distv.hopto.org:8000/disbook/"><img src ="https://i.postimg.cc/SKFBJHZh/teel-me.jpg" height="60px" width="150px" vspace="5px"></a>
+<a href="http://distv.hopto.org:8000/disbook/"><img src ="https://i.postimg.cc/Gm5kK1WW/comunqn.jpg" height="60px" width="150px" vspace="5px"></a>
+<a href="http://distv.hopto.org:8000/disbook/"><img src ="https://i.postimg.cc/76DbMsLv/cobweb.jpg" height="60px" width="150px" vspace="5px"></a>
+<a href="http://distv.hopto.org:8000/disbook/"><img src ="https://i.postimg.cc/kXkJv1jj/island.jpg" height="60px" width="150px" vspace="5px"></a>
+<a href="http://distv.hopto.org:8000/disbook/"><img src ="https://i.postimg.cc/DwrTd24Z/angry.jpg" height="60px" width="150px" vspace="5px"></a>
+<a href="http://distv.hopto.org:8000/disbook/"><img src ="https://i.postimg.cc/dtKkq0FQ/an.jpg.jpg" height="60px" width="150px" vspace="5px"></a>
+<a href="http://distv.hopto.org:8000/disbook/"><img src ="https://i.postimg.cc/hj5LyRQz/dempyr.jpg" height="60px" width="150px" vspace="5px"></a>
+<a href="http://distv.hopto.org:8000/disbook/"><img src ="https://i.postimg.cc/zvYCZSp8/boogeyman.jpg" height="60px" width="150px" vspace="5px"></a>
+<a href="http://distv.hopto.org:8000/disbook/"><img src ="https://i.postimg.cc/fyMvSfd5/cragked.jpg" height="60px" width="150px" vspace="5px"></a>
+<a href="http://distv.hopto.org:8000/disbook/"><img src ="http://distv.hopto.org:8000/up/upload/book.jpg" height="60px" width="150px" vspace="5px"></a>
+<h2>   </h2>
+
+
+
+
+
+
+</table></a> <!-- نهاية إعلان محدد --></tr></table></MARQUEE></td></tr></table>
+</td><td background="/bakborleft.gif" width="20">
+</td></tr></table>
+<!-- نهاية إعلانات -->
+  
+   
+  
+  
+  
+  
+  
+
+<!---------------------
+
+
+---------------------->
+
+<!----------  اكواد استايل القنوات  ------------>
+<style type="text/css">
+.grid-container {
+display: grid;
+justify-content: center;
+grid-template-columns: repeat(3, 1fr);
+grid-gap: 5px 5px;
+padding: 10px;
+}
+.grid1-container {
+display: grid;
+justify-content: center;
+grid-template-columns: repeat(1, 1fr);
+grid-gap: 10px;
+
+color: white;
+padding: px;
+}
+h1 {
+text-align: center;
+margin-top: 1px;
+font-weight: bold;
+font-family: "Readex Pro", sans-serif;
+
+}
+
+h2 {
+text-align: center;
+margin-top: 0.5px;
+font-size: 12px;
+color: #8d8daa;
+font-family: "Readex Pro", sans-serif;
+font-weight: bold;
+}
+
+
+
+button {
+ width: 9em;
+ height: 3em;
+ border-radius: 30em;
+ font-size: 15px;
+ font-family: inherit;
+ border: none;
+ position: relative;
+ overflow: hidden;
+ z-index: 1;
+ box-shadow: 6px 6px 12px #c5c5c5,
+             -6px -6px 12px #ffffff;
+}
+
+button::before {
+ content: '';
+ width: 0;
+ height: 5em;
+ border-radius: 30em;
+ position: absolute;
+ top: 0;
+ left: 0;
+ background-image: linear-gradient(to right, #040504 0%, #f2f2e9 100%);
+ transition: .9s ease;
+ display: block;
+ z-index: -1;
+}
+
+button:hover::before {
+ width: 130em;
+}
+
+button {
+border-radius: 10px;
+background-image: linear-gradient(#302e30, #302e30);
+border-color: #302e30;
+padding: 0px;
+margin: 3px;
+height: 80px;
+width:98%;
+cursor: pointer;
+}
+
+ 
+/* Style the buttons */
+.btn {
+  border: none;
+  outline: none;
+  padding: 10px 16px;
+  background-color:#302e30;
+  cursor: pointer;
+  font-size: 18px;
+}
+</style>
+
+<!----------  اكواد استايل التطبيق شامل  ------------>
+<style type="text/css">
+@import url("https://fonts.googleapis.com/css2?family=Readex+Pro&display=swap");
+body {
+padding: 0;
+margin: 0;
+user-select: none;
+list-style: none;
+background: #05050f;
+background-size: cover;
+font-weight: 500;
+font-size: 16px;
+text-align: right;
+margin: auto;
+/* position: relative; */
+direction: ltr;
+
+/**  تغير  خلفيه التطبيق **/
+background-image:#524e51;
+/**  تغير  خلفيه التطبيق **/
+}
+h3 {
+text-align: center;
+/* text-align: right; */
+/* padding-right: 65px; */
+/* padding-top: 2px;
+    font-size: medium; */
+font-weight: 200;
+font-family: "Readex Pro", sans-serif;
+}
+
+header {
+
+
+#i1 {
+position: fixed;
+top: 21px;
+
+right: 17px;
+font-size: 40px;
+}
+#i2 {
+position: fixed;
+top: 21px;
+left: 14px;
+font-size: 40px;
+}
+#i3 {
+position: fixed;
+top: 16px;
+left: 40px;
+font-size: 40px;
+}
+
+#page:hover {
+background: none;
+}
+#page {
+margin-top: 75px;
+width: 100%;
+}
+
+#page a:hover {
+background: #fff;
+}
+#page a {
+height: 50px;
+padding-top: 2px;
+padding-bottom: 2px;
+margin-bottom: 11px;
+border: 2px solid rgba(188, 17, 21, 255);
+border-radius: 15px;
+width: 91%;
+display: inline-block;
+margin-right: 14px;
+margin-left: 14px;
+background: white;
+}
+#page a img {
+/*height:50px;
+margin-right:10px;
+width:50px;
+padding-bottom:4px;
+*/
+height: 50px;
+margin-right: 10px;
+width: 50px;
+padding-bottom: 4px;
+}
+#page a p {
+text-align: center;
+font-family: "Readex Pro", sans-serif;
+/* position: absolute; */
+/* margin-right: 20px; */
+/* margin-top: -40px; */
+display: inline-block;
+font-weight: 550;
+color: rgba(188, 17, 21, 255);
+}
+</style>
+
+
+  
+<button onclick="window.open('http://distv.hopto.org:8000/live/')">
+ <img src="https://i.postimg.cc/MpNPhK1Q/distvlive.png" height="60px" width="160px" align="left"><img src="https://i.postimg.cc/yd7G14PG/live.jpg" style="border-radius: 8px;" align="right" height="50px" width="120px" vspace="5px"/>
+<h2></h2>
+</button>
+
+<button onclick="window.open('http://distv.hopto.org:8000/movies1/')">
+<img src="https://i.postimg.cc/N0nJvGTX/hgk.png" align="left" height="60px" width="160px" ><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd2b5yZGii_7B0BFUzebFNloy8C6UF1EdGdA&usqp=CAU" style="border-radius: 8px;" align="right" height="50px" width="120px" vspace="3px"/>
+<h2></h2>
+</button>
+
+<button onclick="window.open('http://distv.hopto.org:8000/serie/')">
+<img src="https://i.postimg.cc/VLxr0xw8/SERIES.png" align="left" height="60px" width="160px" ><img src="https://i.postimg.cc/R0bzD09R/seris.jpg" style="border-radius: 8px;" align="right" height="50px" width="120px" vspace="3px"/>
+<h2></h2>
+</button>
+
+<button onclick="window.open('http://distv.hopto.org:8000/disbook/')">
+<img src="https://i.postimg.cc/66RN5gMR/diiis.png" align="left" height="60px" width="160px" ><img src="http://distv.hopto.org:8000/up/upload/book.jpg" style="border-radius: 8px;" align="right" height="50px" width="120px" vspace="3px"/>
+<h2></h2>
+</button> 
+
+<button onclick="window.open('http://distv.hopto.org:8000/apps/vvvv')">
+<img src="https://i.postimg.cc/qRJjVtDj/147852.png" align="left" height="60px" width="160px" ><img src="https://i.postimg.cc/t4TCgwrY/app.jpg" style="border-radius: 8px;" align="right" height="50px" width="120px" vspace="3px"/>
+<h2></h2>
+</button>
+
+<button onclick="window.open('https://www.google.dz/?hl=ar')">
+<img src="https://i.postimg.cc/9Xk7PR2Z/google.png" style="border-radius: 8px;" align="left" height="60px" width="160px"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSFHuNlYXoZOWppPT7UJ8zbG237KME4SyY1w&usqp=CAU" style="border-radius: 8px;" align="right" height="50px" width="120px" vspace="3px"/>
+<h2></h2>
+</button>
+
+<button onclick="window.open('https://www.youtube.com/watch?v=-82PI9ECA-Y')">
+<img src="https://i.postimg.cc/KvdtbKsx/13.png" align="left" height="60px" width="160px" ><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQV7aBwS211HL3CgMZgd24cRQkxomT_7epeQ&usqp=CAU" style="border-radius: 8px;" align="right" height="50px" width="120px" vspace="3px"/>
+<h2></h2>
+</button> 
+  
+<button onclick="window.open('https://m.facebook.com/login/?locale=ar_AR&refsrc=deprecated')">
+<img src="https://i.postimg.cc/tTSVM7x0/facebook.png" align="left" height="60px" width="160px" > <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6BATTlWvwUjgFNsvUWztQcOU2B8D2BWHvTg&usqp=CAU" style="border-radius: 8px;" align="right" height="50px" width="120px" vspace="3px"/>
+<h2></h2>
+</button>
+
+
+  
+
+
+
+  
+  
+  
+</div>  
+</div>
+
+<script>
+function openNav() {
+document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+}
+</script>
+</div>
+</div>
+</div>
+</header>
+</body>
+
+
+
+<?php
+
+$visitors = file_get_contents('visitors.txt');
+$visitors = $visitors+1;
+
+file_put_contents('visitors.txt',$visitors);
+
+?> ​
+
+
+
+
+
+
